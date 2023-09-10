ac67924bbc30fcc525db0ecd9c2c293b46214677
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-10 10:27:44 -0400

    Created Post JPA Entity

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/model/Post.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/model/Post.java
@@ -1,4 +1,33 @@
 package com.springboot.blog.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+// Hibernate uses proxies to create objects and hence you need no args
+@NoArgsConstructor
+
+// specify class as entity
+@Entity
+@Table(
+        name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
 public class Post {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
 }
